c416ea4e64239ad7207d1c619a7ba27e75b860e1
Ali Foroughi <foroughi.ali@gmail.com>
2024-04-04 22:51:05 +0000

    change of plan, now ext listens to type event

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,88 @@
 
 import * as vscode from 'vscode';
 
-export interface IJumpArg {
+export interface IJump {
     include: boolean;
     pattern: string,
-    direction: ">" | "<" | "|"
+    direction: "left" | "right" | "both"
 }
 
 export function activate(context: vscode.ExtensionContext) {
 
-    context.subscriptions.push(vscode.commands.registerCommand('vmotion.jumpTo', JumpTo));
+    var capture = false;
+    var capturedKeys = "";
 
+    var interval = setInterval(() => {
+
+        capture = false;
+        capturedKeys = "";
+
+    }, 3000);
+
+    context.subscriptions.push(vscode.commands.registerCommand('vmotion', () => {
+        capture = true;
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('cancel vmotion', () => {
+        capture = false;
+    }));
+
+    vscode.commands.registerCommand("type", async (e) => {
+        if (vscode.window.activeTextEditor) {
+            var value = e.text;
+
+            interval.refresh();
+
+            if (capture) {
+
+                capturedKeys += value;
+                var motionFound = false;
+
+                if (capturedKeys.startsWith("]") && capturedKeys.length > 1) {
+                    JumpTo({ include: false, direction: "right", pattern: capturedKeys[1] });
+                    motionFound = true;
+                }
+                else if (capturedKeys.startsWith("[") && capturedKeys.length > 1) {
+                    JumpTo({ include: false, direction: "left", pattern: capturedKeys[1] });
+                    motionFound = true;
+                }
+                else if (capturedKeys.startsWith(">") && capturedKeys.length > 1) {
+                    JumpTo({ include: true, direction: "right", pattern: capturedKeys[1] });
+                    motionFound = true;
+                }
+                else if (capturedKeys.startsWith("<") && capturedKeys.length > 1) {
+                    JumpTo({ include: true, direction: "left", pattern: capturedKeys[1] });
+                    motionFound = true;
+                }
+                else if (capturedKeys.startsWith("|") && capturedKeys.length > 1) {
+                    JumpTo({ include: false, direction: "both", pattern: capturedKeys[1] });
+                    motionFound = true;
+                }
+
+                if (motionFound) {
+                    console.log("motion executed : " + capturedKeys);
+                    capturedKeys = "";
+                    capture = false;
+                }
+
+                return;
+            }
+
+            const snippet = new vscode.SnippetString(value);
+            const selectionsSorted = Array.from(vscode.window.activeTextEditor.selections).sort((a, b) => -a.start.compareTo(b.start));
+            for (const selection of selectionsSorted) {
+                await vscode.window.activeTextEditor.insertSnippet(snippet, selection);
+            }
+
+        }
+    });
 }
 
 export function deactivate() { }
 
-function findNext(editor: vscode.TextEditor, pos: vscode.Position, args: IJumpArg) {
+function findNext(editor: vscode.TextEditor, args: IJump) {
 
-    var startingPos = editor.document.offsetAt(pos);
+    var startingPos = editor.document.offsetAt(editor.selection.start);
 
     var sliceContent = editor.document.getText().slice(startingPos);
     var i = startingPos;
@@ -27,12 +92,12 @@ function findNext(editor: vscode.TextEditor, pos: vscode.Position, args: IJumpAr
         i += args.pattern.length;
     }
 
-    return sliceContent.startsWith(args.pattern) ? editor.document.positionAt(args.include ? i + 1 : i) : pos;
+    return sliceContent.startsWith(args.pattern) ? editor.document.positionAt(args.include ? i + 1 : i) : editor.selection.start;
 }
 
-function findPrevious(editor: vscode.TextEditor, pos: vscode.Position, args: IJumpArg) {
+function findPrevious(editor: vscode.TextEditor, args: IJump) {
 
-    var startingPos = editor.document.offsetAt(pos);
+    var startingPos = editor.document.offsetAt(editor.selection.start);
 
     var sliceContent = editor.document.getText().slice(0, startingPos);
     var i = startingPos;
@@ -42,42 +107,36 @@ function findPrevious(editor: vscode.TextEditor, pos: vscode.Position, args: IJu
         i -= args.pattern.length;
     }
 
-    return sliceContent.endsWith(args.pattern) ? editor.document.positionAt(args.include ? i - 1 : i ) : pos;
+    return sliceContent.endsWith(args.pattern) ? editor.document.positionAt(args.include ? i - 1 : i) : editor.selection.start;
 
 }
 
-function find(editor: vscode.TextEditor, pos: vscode.Position, args: IJumpArg) {
-    
-    var startingPos = editor.document.offsetAt(pos);
-    var next = findNext(editor , pos , args);
-    var pre = findPrevious(editor , pos , args);
+function find(editor: vscode.TextEditor, args: IJump) {
 
-    if(pos === next)
-    {
-        return pre;
+    var startingPos = editor.document.offsetAt(editor.selection.start);
+    var next = findNext(editor, args);
+    var pre = findPrevious(editor, args);
+
+    if (editor.selection.start === next) {
+        editor.selection = getSelectionFromPosition(pre);
     }
 
-    if(pos === pre)
-    {
-        return next;
+    if (editor.selection.start === pre) {
+        editor.selection = getSelectionFromPosition(next);
     }
-    
+
     return Math.abs(editor.document.offsetAt(next) - startingPos) < Math.abs(editor.document.offsetAt(pre) - startingPos) ? next : pre;
 
 }
 
-function getSelectionFromPosition(pos: vscode.Position) {
-    return new vscode.Selection(pos, pos);
-}
-
-function JumpTo(args: IJumpArg) {
+function JumpTo(args: IJump) {
 
     if (!args.include) {
         args.include = false;
     }
 
     if (!args.direction) {
-        args.direction = "|";
+        args.direction = "both";
     }
 
     if (!args.pattern) {
@@ -94,17 +153,21 @@ function JumpTo(args: IJumpArg) {
     let sel = editor.selection;
     var loc = sel.start;
 
-    if (args.direction === ">") {
-        editor.selection = getSelectionFromPosition(findNext(editor, loc, args));
+    if (args.direction === "right") {
+        editor.selection = getSelectionFromPosition(findNext(editor, args));
     }
-    else if (args.direction === "<") {
-        editor.selection = getSelectionFromPosition(findPrevious(editor, loc, args));
+    else if (args.direction === "left") {
+        editor.selection = getSelectionFromPosition(findPrevious(editor, args));
     }
     else {
-        editor.selection = getSelectionFromPosition(find(editor, loc, args));
+        editor.selection = getSelectionFromPosition(find(editor, args));
     }
 
     console.log(args);
 
 
+}
+
+function getSelectionFromPosition(pos: vscode.Position) {
+    return new vscode.Selection(pos, pos);
 }
